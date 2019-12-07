071445b4238bddd96bc9e30d9d24ac7833c5877b
a1004728356 <a1004728356@qq.com>
2019-12-07 20:49:58 +0800

    add

--- 第五阶段/DEVOPS/myrequest.py
+++ 第五阶段/DEVOPS/myrequest.py
@@ -4,5 +4,6 @@ import requests
 # r=requests.get('http://www.baidu.com/s',params=payload)
 #相当于访问http://www.baidu.com/s?wd=centos7
 #################################################
+#请求时，加上指定的头部信息
 header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
 r=requests.get('http://127.0.0.1',headers=header)
