3c6e6bbb258aa17a44ef352caf19a53c2785a272
a1004728356 <a1004728356@qq.com>
2019-12-07 20:58:05 +0800

    add

--- 第五阶段/DEVOPS/myrequest.py
+++ 第五阶段/DEVOPS/myrequest.py
@@ -7,3 +7,7 @@ import requests
 #请求时，加上指定的头部信息
 header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
 r=requests.get('http://127.0.0.1',headers=header)
+########################################################0
+#data用于提交表单数据
+data={'usename':'xxx','password':'yyyyyy'}
+r=requests.post('http://login.baidu.com',data=data)
