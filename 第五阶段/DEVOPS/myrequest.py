dfab5bc87f7eec564e626af74452e5adf98d8b47
a1004728356 <a1004728356@qq.com>
2019-12-07 21:13:52 +0800

    add

--- 第五阶段/DEVOPS/myrequest.py
+++ 第五阶段/DEVOPS/myrequest.py
@@ -14,5 +14,9 @@ r=requests.post('http://login.baidu.com',data=data)
 #######################################
 #获得网页信息
 r=requests.get('http://www.baidu.com/')
-data=r.text#查看页面内容
-print(data)
+data=r.text#查看页面内容，默认的文字编码是ISO8859-1
+r.encoding('utf8')
+#######################
+r2=requests.get('http://g.hiphotos.baidu.com/exp/w=500/sign=67597f61b951f819f125034aeab54a76/09fa513d269759ee63269981bbfb43166d22df02.jpg')
+with open('a.jpg','wb') as fobj:
+    fobj.write(r2.content)#content是bytes格式
