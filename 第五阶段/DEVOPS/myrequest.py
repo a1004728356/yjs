c8c4148569144ca1d49fd0ac5af4697eb534520d
a1004728356 <a1004728356@qq.com>
2019-12-07 20:37:36 +0800

    add

--- 第五阶段/DEVOPS/myrequest.py
+++ 第五阶段/DEVOPS/myrequest.py
@@ -1,5 +1,5 @@
 #'pip3 install requests'
-import request
+import requests
 payload={'wd':'centos7'}
-r=request.get('http://www.baidu.com/s',params=payload)
+r=requests.get('http://www.baidu.com/s',params=payload)
 #相当于访问http://www.baidu.com/s?wd=centos7
